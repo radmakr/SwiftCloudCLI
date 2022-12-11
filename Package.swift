e3d33ee07cfb857c599639602189ab80db9c5a2a
Thomas Rademaker <thomas@sparrowtek.com>
2022-12-11 16:54:46 -0500

    remove comments

--- Package.swift
+++ Package.swift
@@ -8,8 +8,6 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "swiftcloud",
             dependencies: [
